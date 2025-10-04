7e7e17cf7d8312464fb5a436baf274e13a4b33c6
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-04 15:59:48 -0600

    feat(components): add StatusBadge component for displaying status indicators

---
+++ src/components/StatusBadge.tsx
@@ -0,0 +1,10 @@
+import { Status } from "@/types/status";
+
+function StatusBadge({ status }: { status: Status }) {
+  return (
+    <span className='inline-flex max-w-max animate-shine items-center justify-center rounded-full border border-neutral-200 bg-[linear-gradient(110deg,#f5f5f5,45%,#d4d4d4,55%,#f5f5f5)] bg-[length:200%_100%] px-2 py-0.5 font-mono text-[10px] font-medium text-nowrap text-neutral-700 transition-colors'>
+      {status}
+    </span>
+  );
+}
+export { StatusBadge };
