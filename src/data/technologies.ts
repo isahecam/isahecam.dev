1e1912dd6fe195570fc38f18176e5cc870612a93
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-04 16:02:36 -0600

    feat(data): add technologies configuration with tech stack icons

---
+++ src/data/technologies.ts
@@ -0,0 +1,41 @@
+import { Technology } from "@/types/technology";
+import {
+  Nextjs,
+  TypeScript,
+  TailwindCSS,
+  ShadcnUI,
+  Supabase,
+  ReactJS,
+  GoogleCalendar,
+} from "@/components/icons";
+
+export const TECHNOLOGIES: Technology[] = [
+  {
+    icon: Nextjs,
+    name: "Next.js",
+  },
+  {
+    icon: TypeScript,
+    name: "TypeScript",
+  },
+  {
+    icon: TailwindCSS,
+    name: "Tailwind CSS",
+  },
+  {
+    icon: ShadcnUI,
+    name: "shadcn/ui",
+  },
+  {
+    icon: Supabase,
+    name: "Supabase",
+  },
+  {
+    icon: ReactJS,
+    name: "React.js",
+  },
+  {
+    icon: GoogleCalendar,
+    name: "Google Calendar API",
+  },
+];
