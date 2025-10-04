e9e399116b73e27a6459560a9165e5b4ff07366b
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-04 15:59:07 -0600

    feat(components): add Notice component for work-in-progress states

---
+++ src/components/Notice.tsx
@@ -0,0 +1,8 @@
+function Notice() {
+  return (
+    <article className='relative flex items-center justify-center rounded-md border border-dashed border-[#4F4F4F] px-3 py-4 shadow-sm dark:border-neutral-800'>
+      <p className='font-mono text-sm'>I&apos;m working on it... ✍️</p>
+    </article>
+  );
+}
+export { Notice };
