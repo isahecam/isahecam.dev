49831a342be1dd781f82cfc010f1feacc387a8af
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-04 15:59:31 -0600

    feat(components): add SocialLink component for external social media links

---
+++ src/components/SocialLink.tsx
@@ -0,0 +1,14 @@
+import { SocialLink as Props } from "@/types/social-link";
+import Link from "next/link";
+
+export default function SocialLink({ to, label, icon: Icon }: Props) {
+  return (
+    <Link
+      href={to}
+      target='_blank'
+      className='inline-flex items-center gap-x-2 text-xs font-bold text-black/70 underline-offset-4 hover:underline md:text-sm'>
+      <Icon className='size-4' />
+      {label}
+    </Link>
+  );
+}
