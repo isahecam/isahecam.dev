eadadc0458b2ec175fd05ef7da9e492d04539ef9
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-04 16:02:15 -0600

    feat(data): add navigation links configuration

---
+++ src/data/navigation.ts
@@ -0,0 +1,19 @@
+import { NavLink } from "@/types/nav-link";
+
+export const NAV_LINKS: NavLink[] = [
+  {
+    id: 1,
+    to: "/",
+    label: "Home",
+  },
+  {
+    id: 2,
+    to: "/blog",
+    label: "Blog",
+  },
+  {
+    id: 3,
+    to: "/gallery",
+    label: "My Gallery",
+  },
+];
