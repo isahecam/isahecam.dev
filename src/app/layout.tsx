b75e00333fbff63e3c4fe232636f8f85279f9b92
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-04 16:00:36 -0600

    refactor(components): fix Footer component import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
-import Footer from "@/components/icons/Footer";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
