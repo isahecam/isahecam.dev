f1c8c5414c933d9322eeb0aafe129561b466c312
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-04 15:57:11 -0600

    feat(navbar): add main navigation component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { NAV_LINKS } from "@/data/navigation";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav aria-label='Main navigation'>
+      <ul className='flex w-full items-center justify-center gap-x-6 pt-12 font-mono'>
+        {NAV_LINKS.map(({ id, to, label }) => (
+          <li className='contents' key={id}>
+            <Link
+              className='text-gray-500 underline-offset-4 hover:text-gray-600 hover:underline'
+              key={id}
+              href={to}>
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
