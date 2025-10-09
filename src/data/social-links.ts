584a7afa973e616efcccacdf4bbd769b3b310e75
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-09 13:51:37 -0600

    feat(social-links): add LinkedIn and reorder social links

--- src/data/social-links.ts
+++ src/data/social-links.ts
@@ -2,8 +2,19 @@ import { SocialLink } from "@/types/social-link";
 import { GitHub } from "@/components/icons/GitHub";
 import { Instagram } from "@/components/icons/Instagram";
 import { X } from "@/components/icons/X";
+import { LinkedIn } from "@/components/icons/LinkedIn";
 
 export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    to: "https://github.com/isahecam",
+    label: "GitHub",
+    icon: GitHub,
+  },
+  {
+    to: "https://www.linkedin.com/in/isahir-hernandez",
+    label: "LinkedIn",
+    icon: LinkedIn,
+  },
   {
     to: "https://instagram.com/isahecam",
     label: "Instagram",
@@ -13,10 +24,5 @@ export const SOCIAL_LINKS: SocialLink[] = [
     to: "https://twitter.com/isahecam",
     label: "Twitter",
     icon: X,
-  },
-  {
-    to: "https://github.com/isahecam",
-    label: "GitHub",
-    icon: GitHub,
   },
 ];
