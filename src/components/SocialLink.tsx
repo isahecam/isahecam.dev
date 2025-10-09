c703f8cb800c6e5f2b69fc05bd44cbbe4d2b4970
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-09 13:50:58 -0600

    style(SocialLink): add max-w-max constraint to link wrapper

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -6,7 +6,7 @@ export default function SocialLink({ to, label, icon: Icon }: Props) {
     <Link
       href={to}
       target='_blank'
-      className='inline-flex items-center gap-x-2 text-xs font-bold text-black/70 underline-offset-4 hover:underline md:text-sm'>
+      className='inline-flex max-w-max items-center gap-x-2 text-xs font-bold text-black/70 underline-offset-4 hover:underline md:text-sm'>
       <Icon className='size-4' />
       {label}
     </Link>
