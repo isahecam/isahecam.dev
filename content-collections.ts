ae4d40a8c415fc047a65c0b1adf5cd56506e5ed8
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-11 19:48:53 -0600

    feat(content): add content-collections configuration for posts

---
+++ content-collections.ts
@@ -0,0 +1,22 @@
+import { PostSchema } from "@/schemas/PostSchema";
+import { defineCollection, defineConfig } from "@content-collections/core";
+import { compileMarkdown } from "@content-collections/markdown";
+
+const posts = defineCollection({
+  name: "posts",
+  directory: "src/posts",
+  include: "**/*.md",
+  schema: PostSchema,
+
+  transform: async (document, context) => {
+    const html = await compileMarkdown(context, document);
+    return {
+      ...document,
+      html,
+    };
+  },
+});
+
+export default defineConfig({
+  collections: [posts],
+});
