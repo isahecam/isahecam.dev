de863b03b3f9ea03f1959a999ffd7f1390db1b91
Brandon Isahir Hernández Camacho <isahecam@gmail.com>
2025-10-11 19:50:13 -0600

    refactor(home): rename PostsList component to Posts and remove padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import { Experience } from "@/sections/Experience";
 import { Hero } from "@/sections/Hero";
-import { PostsList } from "@/sections/PostsList";
+import { Posts } from "@/sections/Posts";
 import { Projects } from "@/sections/Projects";
 
 export default function Home() {
   return (
-    <main className='flex w-full motion-preset-slide-right flex-col px-6 py-10 motion-duration-2000'>
+    <main className='flex w-full motion-preset-slide-right flex-col motion-duration-2000'>
       <Hero />
       <Experience />
       <Projects />
-      <PostsList />
+      <Posts />
     </main>
   );
 }
