3fd6db42146ecfde561994ed064dab4fab418f64
Brandon Hernández <isahecam@gmail.com>
2025-10-13 15:50:19 -0600

    feat(data): add formatted date display to experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -9,7 +9,7 @@ export const MY_EXPERIENCE: Experience[] = [
     role: "Software Engineering Intern",
     aboutRole:
       "As a Software Engineering Intern at RC Ingeniería y Diseño, I contributed to the development of a comprehensive web solution for the commercial sector, leveraging modern technologies such as Next.js, React.js, TypeScript, Tailwind CSS and Supabase Storage. I was actively involved in all stages of the development lifecycle from requirements analysis and stakeholder collaboration to system design, implementation, and production deployment strengthening my skills in full-stack development, project management, and technical communication.",
-    startDate: formatDate(new Date("2025-06-05")),
-    endDate: formatDate(new Date("2025-09-06")),
+    startDate: formatDate(new Date("2025-06-05"), "MMMM, YYYY", "en-US"),
+    endDate: formatDate(new Date("2025-09-06"), "MMMM, YYYY", "en-US"),
   },
 ];
