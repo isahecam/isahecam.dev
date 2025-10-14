7c6fb338f7a987ab65cfb18674767c7e1b1de0b6
Brandon Hernández <isahecam@gmail.com>
2025-10-14 13:13:44 -0600

    feat(ui): add monospace font to projects section title

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,7 +4,7 @@ import { MY_PROJECTS } from "@/data/my-projects";
 function Projects() {
   return (
     <section className='mt-16' id='projects'>
-      <h2 className='mb-8 text-xl font-bold text-black'>Projects</h2>
+      <h2 className='mb-8 font-mono text-xl font-bold text-black'>Projects</h2>
       <div className='grid grid-cols-1 gap-y-6'>
         {MY_PROJECTS.map(project => (
           <ProjectCard key={project.id} {...project} />
