1df9ff6910fbfa8df352df44c43fdca57a51dc63
Brandon Hernández <isahecam@gmail.com>
2025-10-14 14:09:09 -0600

    feat(ui): add metadata to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,31 @@
 import PostList from "@/components/PostList";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Brandon Hernández | Blog",
+  description:
+    "These are some articles I've written about web development, life, and more.",
+  keywords: [
+    "Brandon Hernández",
+    "isahecam",
+    "Portfolio",
+    "IT Engineer",
+    "Full Stack Developer",
+    "Developer",
+    "Web Development",
+  ],
+  authors: [{ name: "Brandon Hernández", url: "https://isahecam.dev" }],
+  creator: "Brandon Hernández",
+  openGraph: {
+    title: "Brandon Hernández | Blog",
+    description:
+      "These are some articles I've written about web development, life, and more.",
+    url: "https://isahecam.dev/blog",
+    siteName: "Brandon Hernández | Blog",
+    locale: "en-US",
+    type: "website",
+  },
+};
 
 export default function Blog() {
   return (
