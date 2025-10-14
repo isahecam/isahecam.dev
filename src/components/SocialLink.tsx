e09487b9e94101f9b08a2665da222b4a1729f2d8
Brandon Hernández <isahecam@gmail.com>
2025-10-14 12:47:07 -0600

    feat(ui): add rel noopener noreferrer to social link

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -6,6 +6,7 @@ export default function SocialLink({ to, label, icon: Icon }: Props) {
     <Link
       href={to}
       target='_blank'
+      rel='noopener noreferrer'
       className='inline-flex max-w-max items-center gap-x-2 text-xs font-bold text-black/70 underline-offset-4 hover:underline md:text-sm'>
       <Icon className='size-4' />
       {label}
