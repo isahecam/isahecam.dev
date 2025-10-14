d3e2c58ea3ba4b06aed8fc1a360b152e8ef5a868
Brandon Hernández <isahecam@gmail.com>
2025-10-14 13:29:07 -0600

    feat(ui): update styling in post card component

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -11,37 +11,26 @@ export default function PostCard({
   _meta,
 }: Pick<Post, "title" | "summary" | "date" | "category" | "_meta">) {
   return (
-    <article className='w-full rounded-md border border-dashed border-neutral-500 p-4'>
+    <article className='w-full rounded-md border border-gray-700 p-4'>
       <header className='mb-3 flex flex-col space-y-1'>
         <a
           href={`/blog/${_meta.path}`}
-          className='group flex items-center space-x-1 font-medium underline-offset-4 duration-150 hover:underline hover:opacity-70'>
+          className='group flex items-center space-x-1 font-medium text-gray-900 underline-offset-4 transition-colors duration-150 hover:text-gray-700 hover:underline'>
           <h3 className='font-mono font-bold'>{title}</h3>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='14'
-            height='14'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            className='duration-150 group-hover:translate-x-[2px]'>
-            <path d='m9 18 6-6-6-6'></path>
-          </svg>
+          <ChevronRight
+            size={14}
+            className='duration-150 group-hover:translate-x-[1.5px]'
+          />
         </a>
-        <p className='font-mono text-sm text-pretty text-[#4F4F4F]'>
-          {summary}
-        </p>
+        <p className='font-mono text-sm text-pretty text-gray-700'>{summary}</p>
       </header>
       <div className='flex items-center justify-between'>
         <div className='flex gap-x-2'>
-          <span className='inline-flex max-w-max items-center gap-x-2 rounded-md border border-neutral-300 bg-amber-50 px-2 py-1 text-xs font-medium text-nowrap text-black'>
+          <span className='inline-flex max-w-max items-center gap-x-2 rounded-md border border-gray-400 bg-yellow-50 px-2 py-1 text-xs font-medium text-nowrap text-black'>
             <Leaf size={14} />
             <span className='font-mono text-xs font-medium'>{category}</span>
           </span>
-          <span className='inline-flex max-w-max items-center gap-x-2 rounded-md border border-neutral-300 bg-amber-50 px-2 py-1 text-xs font-medium text-nowrap text-black'>
+          <span className='inline-flex max-w-max items-center gap-x-2 rounded-md border border-gray-400 bg-yellow-50 px-2 py-1 text-xs font-medium text-nowrap text-black'>
             <Calendar size={14} />
             <time
               className='font-mono text-xs font-medium'
@@ -54,11 +43,11 @@ export default function PostCard({
 
         <Link
           href={`/blog/${_meta.path}`}
-          className='group flex items-center justify-center space-x-1 text-sm tracking-tight opacity-70 transition-opacity duration-100 hover:opacity-100'>
+          className='group flex items-center justify-center space-x-1 text-sm tracking-tight text-gray-900 duration-150 hover:text-gray-700'>
           <span className='font-mono text-sm'>Read more</span>
           <ChevronRight
             size={14}
-            className='duration-150 group-hover:translate-x-[2px]'
+            className='duration-150 group-hover:translate-x-[1.5px]'
           />
         </Link>
       </div>
