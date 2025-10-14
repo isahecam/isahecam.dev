85702f0036f0a03ec8b3560306b8d99f48e52964
Brandon Hernández <isahecam@gmail.com>
2025-10-14 13:13:22 -0600

    feat(ui): update styling in tech badge component

--- src/components/TechBadge.tsx
+++ src/components/TechBadge.tsx
@@ -2,7 +2,7 @@ import { Technology } from "@/types/technology";
 
 function TechBadge({ icon: Icon, name }: Technology) {
   return (
-    <span className='inline-flex max-w-max items-center gap-x-2 rounded-md border border-neutral-300 bg-amber-50 px-2 py-1 text-xs font-medium text-nowrap text-black'>
+    <span className='inline-flex max-w-max items-center gap-x-2 rounded-md border border-gray-400 bg-yellow-50 px-2 py-1 font-mono text-xs font-medium text-nowrap text-black'>
       <Icon className='size-4' />
       {name}
     </span>
