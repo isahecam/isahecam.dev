8e96732c69794ed2824e03f16a3659082100995a
Brandon Hernández <isahecam@gmail.com>
2025-10-21 22:35:03 -0600

    fix(schema): change date type from coerce to iso in PostSchema

--- src/schemas/PostSchema.ts
+++ src/schemas/PostSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const PostSchema = z.object({
   title: z.string(),
   summary: z.string(),
-  date: z.coerce.date(),
+  date: z.iso.date(),
   category: z.enum(["Tutorial", "Article", "News", "Life"]).default("Life"),
   published: z.boolean().default(false),
 });
