701115d9e325800e85fe5420212c576111c3127e
Brandon Hernández <isahecam@gmail.com>
2025-10-21 22:09:55 -0600

    feat(ui): enhance Navbar component with dynamic link styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import { NAV_LINKS } from "@/data/navigation";
+import { cn } from "@/utils/cn";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav aria-label='Main navigation'>
       <ul className='flex w-full items-center justify-center gap-x-6 pt-12 font-mono'>
         {NAV_LINKS.map(({ id, to, label }) => (
           <li className='contents' key={id}>
             <Link
-              className='text-gray-700 underline-offset-4 transition-colors duration-150 hover:text-gray-500 hover:underline'
+              className={cn(
+                "text-gray-700 transition-colors duration-150 hover:text-gray-500",
+                pathname === to && "pointer-events-none font-bold",
+              )}
               key={id}
               href={to}>
               {label}
