1a8b906d091013c8411b022905216d9c9323f563
Brandon Hernández <isahecam@gmail.com>
2026-04-23 13:51:16 -0600

    refactor(layout): reorganize layout structure and integrate ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 
-import "./globals.css";
 import { cn } from "@/lib/utils";
+
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Header } from "@/shared/components/layout/header";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -21,8 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" className={cn("antialiased", "font-mono", geistMono.className)}>
-      <body className="flex min-h-full flex-col">{children}</body>
+    <html lang="es" suppressHydrationWarning className={cn("antialiased", "font-mono", geistMono.className)}>
+      <body className="flex min-h-full flex-col">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Header />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
