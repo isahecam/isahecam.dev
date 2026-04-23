9f28ece0c264591d3208db61d1dde228c78c703b
Brandon Hernández <isahecam@gmail.com>
2026-04-23 16:37:31 -0600

    feat(components): create Hero component to showcase personal information and links

---
+++ modules/home/sections/hero.tsx
@@ -0,0 +1,62 @@
+import { FileTextIcon } from "lucide-react";
+import Link from "next/link";
+
+import { PromptTerminalMeta } from "@/modules/home/components/prompt-terminal-meta";
+import { WorkStatus } from "@/modules/home/components/work-status";
+import { GitHub } from "@/shared/components/icons/github";
+import { LinkedIn } from "@/shared/components/icons/linkedin";
+import { Heading } from "@/shared/components/typography/heading";
+import { Paragraph } from "@/shared/components/typography/paragraph";
+import { buttonVariants } from "@/shared/components/ui/button";
+import { NativeTypewriter } from "@/shared/components/uitripled/native-typewriter-shadcnui";
+
+export function Hero() {
+  return (
+    <section className="flex flex-col gap-8">
+      <header className="space-y-4">
+        <PromptTerminalMeta />
+        <WorkStatus />
+      </header>
+
+      <article className="flex flex-col gap-6">
+        <header className="space-y-2">
+          <Heading level={1} className="mb-2">
+            Brandon Hernández
+          </Heading>
+          <NativeTypewriter
+            className="block h-5.5 text-accent-foreground dark:text-accent"
+            content={["IT Engineer", "Full Stack Developer"]}
+            loop
+          />
+          <Paragraph className="max-w-prose text-muted-foreground dark:text-muted-foreground">
+            I'm a <strong>Systems and Industrial Information Technology Engineer</strong> with over a year of experience
+            as a <strong>full-stack developer</strong>. I'm a passionate lifelong learner, driven by creativity and
+            ambition to build meaningful web experiences and solutions.
+          </Paragraph>
+        </header>
+        <footer className="flex gap-2">
+          <Link
+            className={buttonVariants({ variant: "outline", size: "sm" })}
+            href="https://github.com/isahecam"
+            target="_blank">
+            <GitHub />
+            GitHub
+          </Link>
+
+          <Link
+            className={buttonVariants({ variant: "outline", size: "sm" })}
+            href="https://www.linkedin.com/in/isahir-hernandez/"
+            target="_blank">
+            <LinkedIn />
+            LinkedIn
+          </Link>
+
+          <Link className={buttonVariants({ variant: "outline", size: "sm" })} href="/" download>
+            <FileTextIcon />
+            CV
+          </Link>
+        </footer>
+      </article>
+    </section>
+  );
+}
