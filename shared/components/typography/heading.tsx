7e32333c0ea9ee22ef8ab55dbd83690ec0e65e3d
Brandon Hernández <isahecam@gmail.com>
2026-04-23 19:21:25 -0600

    fix(typography): remove unnecessary border from level 2 heading style

--- shared/components/typography/heading.tsx
+++ shared/components/typography/heading.tsx
@@ -10,7 +10,7 @@ export function Heading({ children, level = 1, className, ...props }: Props) {
 
   const sizeMap = {
     1: "scroll-m-20 text-4xl tracking-tight text-balance uppercase",
-    2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+    2: "scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0",
     3: "scroll-m-20 text-2xl font-semibold tracking-tight",
     4: "scroll-m-20 text-xl font-semibold tracking-tight",
     5: "scroll-m-20 text-lg font-semibold tracking-tight",
