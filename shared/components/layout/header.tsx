a54e7458d9568518b46d7d10a5978112c43c4619
Brandon Hernández <isahecam@gmail.com>
2026-04-23 20:02:23 -0600

    style(header): enhance layout structure by wrapping Logo and ThemeToggle in a flex container

--- shared/components/layout/header.tsx
+++ shared/components/layout/header.tsx
@@ -3,9 +3,11 @@ import { ThemeToggle } from "@/shared/components/layout/theme-toggle";
 
 export function Header() {
   return (
-    <header className="fixed flex h-11.5 w-full items-center justify-between border-b border-accent px-5 py-2.5 backdrop-blur-xl">
-      <Logo />
-      <ThemeToggle />
+    <header className="fixed w-full border-b border-accent px-5 py-2.5 backdrop-blur-xl">
+      <div className="mx-auto flex w-full max-w-4xl items-center justify-between">
+        <Logo />
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
