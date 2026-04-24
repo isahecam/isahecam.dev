d16fc47c5bbaaab32e271ca230d22b9d0f44e086
Brandon Hernández <isahecam@gmail.com>
2026-04-23 19:15:22 -0600

    feat(home): add Projects component to Home page for showcasing projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/modules/home/sections/hero";
+import { Projects } from "@/modules/home/sections/projects";
 
 export default function Home() {
   return (
     <main className="px-5 py-25">
       <Hero />
+      <Projects />
     </main>
   );
 }
