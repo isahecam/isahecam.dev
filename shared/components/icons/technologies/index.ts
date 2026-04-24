a7acc5183a633ffa59b8e59699a77a5cb883673b
Brandon Hernández <isahecam@gmail.com>
2026-04-23 19:20:41 -0600

    feat(icons): add new technology icons

---
+++ shared/components/icons/technologies/index.ts
@@ -0,0 +1,7 @@
+export * from "./astro";
+export * from "./drizzle-orm";
+export * from "./neon";
+export * from "./nextjs";
+export * from "./react";
+export * from "./tailwindcss";
+export * from "./typescript";
