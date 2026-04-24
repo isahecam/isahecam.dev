71ed77233bf8882ad280252beb54e2dedbe641b5
Brandon Hernández <isahecam@gmail.com>
2026-04-23 19:28:01 -0600

    style(header): update backdrop blur effect for improved visual clarity

--- shared/components/layout/header.tsx
+++ shared/components/layout/header.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "@/shared/components/layout/theme-toggle";
 
 export function Header() {
   return (
-    <header className="fixed flex h-11.5 w-full items-center justify-between border-b border-accent px-5 py-2.5 backdrop-blur-sm">
+    <header className="fixed flex h-11.5 w-full items-center justify-between border-b border-accent px-5 py-2.5 backdrop-blur-xl">
       <Logo />
       <ThemeToggle />
     </header>
