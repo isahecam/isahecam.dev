aaff3ba8798a18904a1d4bcde0f7060adc77483b
Brandon Hernández <isahecam@gmail.com>
2026-04-27 17:18:50 -0600

    feat(constants): add user profile constants including personal details, bio, and keywords

---
+++ shared/constants/portfolio.constants.tsx
@@ -0,0 +1,36 @@
+import { Portfolio } from "@/shared/types/portfolio.types";
+
+export const ME = {
+  displayName: "Brandon Hernández",
+  firstName: "Brandon",
+  lastName: "Hernández",
+  availability: {
+    status: "success",
+    label: "Open to work",
+  },
+  gender: "male",
+  jobTitle: "IT Engineer and Full Stack Developer",
+  address: "Puebla, MX",
+  nickname: "isahecam",
+  flipSentences: ["I love to code ♥️", "I like the small details", "I'm a quick learner "],
+  bio: (
+    <>
+      I'm a <strong>Systems and Industrial Information Technology Engineer</strong> with over a year of experience as a
+      <strong>full-stack developer</strong>. I'm a passionate lifelong learner, driven by creativity and ambition to
+      build meaningful web experiences and solutions.
+    </>
+  ),
+  summary:
+    "Ingenerio en Sistemas y Tecnologías de la Información Industrial con más de 1 año de experiencia en desarrollo web. Apasionado por la tecnología, explorador y estusiasta de las nuevas tendencias en el mundo de la programación.",
+
+  avatar: "/profile.png",
+  ogImage: "https://assets.isahecam.dev/images/isahecam-avatar-ghibli.webp",
+  keywords: [
+    "isahecam",
+    "isahir hernández",
+    "brandon hernández",
+    "brandon isahir hernández camacho",
+    "systems and industrial information technology engineer",
+    "full-stack developer",
+  ],
+} satisfies Portfolio;
