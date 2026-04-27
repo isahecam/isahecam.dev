1ead2e3f9915217cecdc59836fd75baab3f695a3
Brandon Hernández <isahecam@gmail.com>
2026-04-27 17:20:49 -0600

    feat(constants): remove Full Stack Developer Intern experience entry from experience constants

--- modules/home/constants/experience.constants.ts
+++ modules/home/constants/experience.constants.ts
@@ -9,12 +9,4 @@ export const EXPERIENCES = [
     description:
       "Desarrollo de aplicaciones web y móviles utilizando tecnologías modernas como React, Next.js, Tailwind CSS, TypeScript, entre otras.",
   },
-  {
-    id: 2,
-    role: "Full Stack Developer Intern",
-    company: { name: "Vodafone", website: "https://www.vodafone.com.co", logo: "/logos/vodafone.svg" },
-    period: { startDate: "2024-03-03", endDate: "2024-08-17" },
-    description:
-      "Desarrollo de aplicaciones web y móviles utilizando tecnologías modernas como React, Next.js, Tailwind CSS, TypeScript, entre otras.",
-  },
 ] as const satisfies Experience[];
