a472a2a0baf2cffdd1674d9a649da91049c72a0a
Brandon Hernández <isahecam@gmail.com>
2026-04-29 21:00:54 -0600

    fix(header): adjust header padding for improved layout consistency

--- shared/components/layout/header.tsx
+++ shared/components/layout/header.tsx
@@ -3,8 +3,8 @@ import { ThemeToggle } from "@/shared/components/layout/theme-toggle";
 
 export function Header() {
   return (
-    <header className="fixed z-20 w-full border-b border-primary px-5 py-2.5 backdrop-blur-xl dark:border-primary-foreground">
-      <div className="mx-auto flex w-full max-w-4xl items-center justify-between">
+    <header className="fixed z-20 w-full border-b border-primary backdrop-blur-xl dark:border-primary-foreground">
+      <div className="mx-auto flex w-full max-w-4xl items-center justify-between p-5 lg:px-0">
         <Logo />
         <ThemeToggle />
       </div>
