f75e3ccf094e2da04e26e53bb1db2057fca248ce
Brandon Hernández <isahecam@gmail.com>
2026-05-04 21:23:46 -0600

    feat(social-urls): add constants for public social media profile URLs

--- shared/types/portfolio.types.ts
+++ shared/types/portfolio.types.ts
@@ -7,18 +7,13 @@ export type Status = NonNullable<VariantProps<typeof statusVariants>["variant"]>
 export interface Portfolio {
   availability: {
     status: Status;
-    label: string;
   };
   displayName: string;
   firstName: string;
   lastName: string;
   gender: string;
   jobTitle: string;
-  address: string;
   nickname: string;
-  flipSentences: string[];
-  bio: React.ReactNode;
-  summary: string;
   avatar: string;
   ogImage: string;
   keywords: string[];
