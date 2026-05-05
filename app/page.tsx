629fa4db9c8bed34b3ffaefe3fb71908f4777712
Brandon Hernández <isahecam@gmail.com>
2026-05-04 21:42:46 -0600

    fix(accessibility): add id and tabindex to main element for improved navigation

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ import { Projects } from "@/modules/home/sections/projects";
 
 export default function Home() {
   return (
-    <main className="mx-auto flex max-w-4xl flex-1 flex-col gap-10 px-5 py-25 lg:px-0">
+    <main
+      id="main-content"
+      tabIndex={-1}
+      className="mx-auto flex max-w-4xl flex-1 scroll-mt-24 flex-col gap-10 px-5 py-25 lg:px-0">
       <Hero />
       <Projects />
       <Experience />
