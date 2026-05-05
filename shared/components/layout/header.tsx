ee335d3b1ebdbb20c5f227b627ba8588316999a2
Brandon Hernández <isahecam@gmail.com>
2026-05-04 19:50:33 -0600

    feat(header): add LocaleSwitcher for language selection in header

--- shared/components/layout/header.tsx
+++ shared/components/layout/header.tsx
@@ -1,3 +1,5 @@
+import { changeLocaleAction } from "@/shared/actions/change-locale-action";
+import { LocaleSwitcher } from "@/shared/components/custom/locale-switcher";
 import { Logo } from "@/shared/components/layout/logo";
 import { ThemeToggle } from "@/shared/components/layout/theme-toggle";
 
@@ -6,7 +8,10 @@ export function Header() {
     <header className="fixed z-20 w-full border-b border-primary backdrop-blur-xl dark:border-primary-foreground">
       <div className="mx-auto flex w-full max-w-4xl items-center justify-between p-5 lg:px-0">
         <Logo />
-        <ThemeToggle />
+        <nav className="flex items-center gap-2">
+          <LocaleSwitcher onLocaleChange={changeLocaleAction} />
+          <ThemeToggle />
+        </nav>
       </div>
     </header>
   );
