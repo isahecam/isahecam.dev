71d4c8bcbb7e81b5d5cb20c0c04f8ceb0f18d40c
Brandon Hernández <isahecam@gmail.com>
2026-06-20 16:39:41 -0600

    refactor: update Next.js version and clean up unused utility functions

--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,7 +1,2 @@
-export { mapData } from "./map-data";
-export { findData } from "./find-data";
-export { filterData } from "./filter-data";
-export { groupData } from "./group-data";
-export { indexData } from "./index-data";
 export { createRegistry } from "./create-registry";
 export { formatDate, formatDateRange } from "./format-date";
