e2650e2eedbede9bb6129361f398b190976d2daf
Brandon Hernández <isahecam@gmail.com>
2026-06-23 17:57:36 -0600

    feat(whoami-text-flip): add WhoAmITextFlip component for animated text display

---
+++ modules/home/components/primitives/whoami-text-flip.tsx
@@ -0,0 +1,10 @@
+import { TextFlip } from "@/shared/components/blocks/text-flip";
+
+export function WhoAmITextFlip() {
+  return (
+    <TextFlip>
+      <span>Ingeniero en Sistemas y Tecnologías de la Información Industrial</span>
+      <span>Desarrollador Full Stack</span>
+    </TextFlip>
+  );
+}
