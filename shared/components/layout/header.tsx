fa218a91b4bd712cd8ac2b320b3006d355449ead
Brandon Hernández <isahecam@gmail.com>
2026-06-23 17:55:23 -0600

    refactor(header): improve structure and styling for header component

--- shared/components/layout/header.tsx
+++ shared/components/layout/header.tsx
@@ -8,8 +8,8 @@ export function Header() {
   const currentLocale = useLocale();
 
   return (
-    <header className="sticky top-0 z-20 w-full backdrop-blur-xl dark:border-primary-foreground">
-      <div className="mx-auto flex w-full max-w-4xl items-center justify-between p-5 lg:px-0">
+    <header className="@container-[scroll-state] sticky top-0 z-20 w-full backdrop-blur-xl dark:border-primary-foreground">
+      <div className="navbar mx-auto flex w-full max-w-4xl items-center justify-between p-5 transition-[max-width,border-radius,background-color] duration-200 lg:px-0">
         <Logo />
         <nav className="flex items-center gap-2">
           <ThemeToggle />
