c8f4e775557c0abfcdbe7ba554714d860a3ef9b4
Brandon Hernández <isahecam@gmail.com>
2026-06-24 17:28:29 -0600

    refactor(projects-grid): update grid layout for improved responsiveness

--- modules/projects/projects-grid.tsx
+++ modules/projects/projects-grid.tsx
@@ -5,7 +5,7 @@ export async function ProjectsGrid() {
   const items = await getAllProjects();
 
   return (
-    <div className="grid grid-cols-1 gap-1 md:grid-cols-2">
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {items.map((project) => (
         <ProjectCard key={project._id} project={project} />
       ))}
