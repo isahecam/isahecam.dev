4715de12a6bb8ae8ff05d6fbd5d1441e9a37e70d
Brandon Hernández <isahecam@gmail.com>
2026-06-24 17:27:07 -0600

    refactor(header): update layout styles for improved design consistency

--- shared/components/layout/header.tsx
+++ shared/components/layout/header.tsx
@@ -8,8 +8,8 @@ export function Header() {
   const currentLocale = useLocale();
 
   return (
-    <header className="@container-[scroll-state] sticky top-0 z-20 w-full backdrop-blur-xl dark:border-primary-foreground">
-      <div className="navbar mx-auto flex w-full max-w-4xl items-center justify-between p-5 transition-[max-width,border-radius,background-color] duration-200 lg:px-0">
+    <header className="@container-[scroll-state] sticky top-0 z-20 w-full p-8">
+      <div className="navbar mx-auto flex w-full items-center justify-between rounded-full border border-transparent bg-background px-4 py-2 transition-[max-width,border-radius,background-color] duration-200">
         <Logo />
         <nav className="flex items-center gap-2">
           <ThemeToggle />
