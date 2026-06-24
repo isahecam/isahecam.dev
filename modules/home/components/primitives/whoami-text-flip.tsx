d31f0e2f1d6d09a6b20c77637c7869e57766d184
Brandon Hernández <isahecam@gmail.com>
2026-06-24 17:28:10 -0600

    refactor(whoami-text-flip): restructure component to use roles array for improved maintainability

--- modules/home/components/primitives/whoami-text-flip.tsx
+++ modules/home/components/primitives/whoami-text-flip.tsx
@@ -1,10 +1,21 @@
 import { TextFlip } from "@/shared/components/blocks/text-flip";
 
+const roles = [
+  {
+    _id: "role-1",
+    text: "Ingeniero en Sistemas y Tecnologías de la Información Industrial",
+  },
+  { _id: "role-2", text: "Desarrollador Full Stack" },
+];
+
 export function WhoAmITextFlip() {
   return (
-    <TextFlip>
-      <span>Ingeniero en Sistemas y Tecnologías de la Información Industrial</span>
-      <span>Desarrollador Full Stack</span>
+    <TextFlip className="h-9.5 max-w-96 font-mono font-medium text-pretty text-muted-foreground">
+      {roles.map((role) => (
+        <span key={role._id} className="block">
+          {role.text}
+        </span>
+      ))}
     </TextFlip>
   );
 }
