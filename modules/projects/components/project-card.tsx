dbbd9a7fba80d5b50b70ac83741357a29422c6b9
Brandon Hernández <isahecam@gmail.com>
2026-06-24 21:48:54 -0600

    refactor(project-card): restructure ProjectCard component for improved layout and organization

--- modules/projects/components/project-card.tsx
+++ modules/projects/components/project-card.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import { Project } from "@/modules/projects/types/project.types";
 import { Badge } from "@/shared/components/ui/badge";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/shared/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/shared/components/ui/card";
 
 interface Props {
   project: Project;
@@ -12,35 +12,33 @@ export function ProjectCard({ project }: Props) {
   const { name, description, technologies } = project;
 
   return (
-    <Card className="overflow-hidden border-none shadow-none ring-0">
-      <Image
-        width={1920}
-        height={1440}
-        src={project.cover}
-        alt={project.name}
-        className="relative aspect-video w-full object-cover"
-      />
-      <CardHeader>
-        <CardTitle className="text-balance">{name}</CardTitle>
-        <CardDescription className="col-span-2">{description}</CardDescription>
-      </CardHeader>
-      <CardFooter className="gap-2">
-        {technologies.map((tech) => (
-          <Badge key={tech._id} variant={"outline"} className="h-auto">
-            <Image
-              width={180}
-              height={180}
-              src={tech.icon}
-              alt={tech.name}
-              fetchPriority="high"
-              priority
-              decoding="async"
-              className="size-5"
-            />
-            {tech.name}
-          </Badge>
-        ))}
-      </CardFooter>
+    <Card className="items-center overflow-hidden py-0 sm:flex-row sm:gap-0">
+      <CardContent className="h-56 w-full grow px-0">
+        <Image width={1920} height={1440} src={project.cover} alt={project.name} className="size-full object-cover" />
+      </CardContent>
+      <div className="w-full sm:min-w-[50%]">
+        <CardHeader className="pt-4">
+          <CardTitle>{name}</CardTitle>
+          <CardDescription className="mb-4">{description}</CardDescription>
+        </CardHeader>
+        <CardFooter className="gap-3 py-4">
+          {technologies.map((tech) => (
+            <Badge key={tech._id} variant={"outline"} className="h-auto">
+              <Image
+                width={180}
+                height={180}
+                src={tech.icon}
+                alt={tech.name}
+                fetchPriority="high"
+                priority
+                decoding="async"
+                className="size-5"
+              />
+              {tech.name}
+            </Badge>
+          ))}
+        </CardFooter>
+      </div>
     </Card>
   );
 }
