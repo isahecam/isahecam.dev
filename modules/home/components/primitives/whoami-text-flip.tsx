73ed4c6ab2f9c392973dd66e315439f3098fad90
Brandon Hernández <isahecam@gmail.com>
2026-06-24 22:50:25 -0600

    refactor(whoami-text-flip): update text color for improved readability

--- modules/home/components/primitives/whoami-text-flip.tsx
+++ modules/home/components/primitives/whoami-text-flip.tsx
@@ -10,7 +10,7 @@ const roles = [
 
 export function WhoAmITextFlip() {
   return (
-    <TextFlip className="h-9.5 max-w-96 font-mono font-medium text-pretty text-muted-foreground">
+    <TextFlip className="h-9.5 max-w-96 font-mono text-sm font-medium text-mauve-700 dark:text-mauve-200">
       {roles.map((role) => (
         <span key={role._id} className="block">
           {role.text}
