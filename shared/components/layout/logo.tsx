5e6254f9671b4c1ca851735a2778eb5358adea94
Brandon Hernández <isahecam@gmail.com>
2026-06-25 21:10:18 -0600

    refactor: update internacionalization logo component

--- shared/components/layout/logo.tsx
+++ shared/components/layout/logo.tsx
@@ -1,14 +1,8 @@
-import { getTranslations } from "next-intl/server";
-
 import { Link } from "@/i18n/navigation";
 
 export async function Logo() {
-  const t = await getTranslations("a11y");
   return (
-    <Link
-      href="/"
-      aria-label={t("logo-home")}
-      className="inline-flex items-center text-sm text-muted-foreground dark:text-muted-foreground">
+    <Link href="/" className="inline-flex items-center text-sm text-muted-foreground dark:text-muted-foreground">
       isahecam
       <span className="text-primary dark:text-white">.dev</span>
     </Link>
