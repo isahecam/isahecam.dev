6fcba1ca6b5e6e531e0bef31736af334557a6a64
Brandon Hernández <isahecam@gmail.com>
2026-06-28 21:11:13 -0600

    refactor: remove MapIcon

--- modules/home/components/primitives/location-badge.tsx
+++ modules/home/components/primitives/location-badge.tsx
@@ -1,4 +1,3 @@
-import { MapPinIcon } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 
 import { Badge } from "@/shared/components/ui/badge";
@@ -6,10 +5,5 @@ import { Badge } from "@/shared/components/ui/badge";
 export async function LocationBadge() {
   const t = await getTranslations("home");
 
-  return (
-    <Badge variant={"secondary"}>
-      <MapPinIcon />
-      {t("header.location")}
-    </Badge>
-  );
+  return <Badge variant={"secondary"}>{t("header.location")}</Badge>;
 }
