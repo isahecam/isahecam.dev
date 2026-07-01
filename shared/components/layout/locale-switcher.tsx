54a2872e8a3489abb73d6add66766fea7d681da2
Brandon Hernández <isahecam@gmail.com>
2026-07-01 14:01:07 -0600

    refactor(locale-switcher): enhance locale selection with flag icons

--- shared/components/layout/locale-switcher.tsx
+++ shared/components/layout/locale-switcher.tsx
@@ -7,6 +7,7 @@ import { useTransition } from "react";
 import { usePathname, useRouter } from "@/i18n/navigation";
 import { routing } from "@/i18n/routing";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/shared/components/ui/select";
+import { LOCALE_FLAGS } from "@/shared/constants/locale.constants";
 
 interface Props extends React.ComponentProps<typeof Select> {}
 
@@ -34,11 +35,16 @@ export function LocaleSwitcher({ ...props }: Props) {
         <SelectValue />
       </SelectTrigger>
       <SelectContent align="end" position="popper">
-        {routing.locales.map((cur) => (
-          <SelectItem key={cur} value={cur}>
-            {t("locale", { locale: cur })}
-          </SelectItem>
-        ))}
+        {routing.locales.map((cur) => {
+          const Flag = LOCALE_FLAGS[cur];
+
+          return (
+            <SelectItem key={cur} value={cur}>
+              {Flag && <Flag className="size-4" aria-hidden="true" />}
+              <span>{t("locale", { locale: cur })}</span>
+            </SelectItem>
+          );
+        })}
       </SelectContent>
     </Select>
   );
