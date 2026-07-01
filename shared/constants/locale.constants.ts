2bb723a9773fe6952ef8c9fd883e24ad627c475a
Brandon Hernández <isahecam@gmail.com>
2026-07-01 14:01:16 -0600

    feat(locale): add locale flags for language selection

---
+++ shared/constants/locale.constants.ts
@@ -0,0 +1,7 @@
+import { MXFlag } from "@/shared/components/icons/mx-flag";
+import { USFlag } from "@/shared/components/icons/us-flag";
+
+export const LOCALE_FLAGS = {
+  es: MXFlag,
+  en: USFlag,
+} as const;
