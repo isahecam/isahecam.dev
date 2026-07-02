f4a32c86a128ffdbc132a5ea69908738a294baeb
Brandon Hernández <isahecam@gmail.com>
2026-07-01 18:27:29 -0600

    refactor(background-blurry-blob): update blob dimensions and blur effects for improved visuals

--- shared/components/layout/background-blurry-blob.tsx
+++ shared/components/layout/background-blurry-blob.tsx
@@ -10,13 +10,13 @@ export function BackgroundBlurryBlob({ className, firstBlobColor, secondBlobColo
     <div className="relative inset-0 top-0 right-0 -z-10 w-full">
       <div
         className={cn(
-          "absolute top-0 right-24 h-72 w-72 animate-pop-blob rounded-sm bg-blue-400 p-8 opacity-45 mix-blend-multiply blur-3xl filter",
+          "absolute top-0 right-8 size-40 animate-pop-blob rounded-sm bg-blue-400 p-8 opacity-45 mix-blend-multiply blur-2xl filter sm:right-16 sm:size-56 sm:blur-3xl md:right-24 md:size-72",
           className,
           firstBlobColor,
         )}></div>
       <div
         className={cn(
-          "absolute top-32 right-8 h-72 w-72 animate-pop-blob rounded-sm bg-purple-400 p-8 opacity-45 mix-blend-multiply blur-3xl filter",
+          "absolute top-16 right-2 size-40 animate-pop-blob rounded-sm bg-purple-400 p-8 opacity-45 mix-blend-multiply blur-2xl filter sm:top-24 sm:right-4 sm:size-56 sm:blur-3xl md:top-32 md:right-8 md:size-72",
           className,
           secondBlobColor,
         )}
