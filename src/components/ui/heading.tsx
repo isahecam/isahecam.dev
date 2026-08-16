fee4202f1fb09882451f037c187fb536539bf6e1
Brandon Hernández <isahecam@gmail.com>
2026-08-16 13:09:53 -0600

    feat: add new UI primitive components

---
+++ src/components/ui/heading.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+
+interface Props extends React.HTMLAttributes<HTMLHeadingElement> {
+  children: React.ReactNode;
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
+}
+
+export function Heading({ children, level = 1, className, ...props }: Readonly<Props>) {
+  const Tag: React.ElementType = `h${level}`;
+
+  return (
+    <Tag data-slot="heading" className={cn("tracking-tight text-balance", className)} {...props}>
+      {children}
+    </Tag>
+  );
+}
