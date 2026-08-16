6eb186622ef16f72b66072aceff3508320eb2f40
Brandon Hernández <isahecam@gmail.com>
2026-08-16 13:01:48 -0600

    feat: enhance Next.js configuration with internationalization support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  typedRoutes: true,
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.isahecam.dev",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
