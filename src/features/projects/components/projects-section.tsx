cf7b654effc0bb295b8a7cc6306fea1a83c4ef5e
Brandon Hernández <isahecam@gmail.com>
2026-08-16 13:12:22 -0600

    feat: create ProjectsSection component to display project overview

---
+++ src/features/projects/components/projects-section.tsx
@@ -0,0 +1,19 @@
+import { Heading } from "@/components/ui/heading";
+import { ProjectGridView } from "@/features/projects/components/project-grid-view";
+
+export function ProjectsSection() {
+  return (
+    <section className="space-y-3">
+      <hgroup className="flex flex-col gap-1.5">
+        <Heading
+          level={2}
+          className="text-xs font-semibold tracking-wider text-foreground uppercase"
+        >
+          Projects
+        </Heading>
+      </hgroup>
+
+      <ProjectGridView />
+    </section>
+  );
+}
