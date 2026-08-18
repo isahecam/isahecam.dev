8635efd60379b377626ad9ceb4a708e84dccfae6
Brandon Hernández <isahecam@gmail.com>
2026-08-18 15:44:17 -0600

    chore: add GitHub, LinkedIn, and XformerlyTwitter icon components

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,7 @@
 export * from "./better-auth";
 export * from "./drizzle-orm";
+export * from "./github";
+export * from "./linkedin";
 export * from "./next-js";
 export * from "./shadcn-ui";
+export * from "./x-formerly-twitter";
