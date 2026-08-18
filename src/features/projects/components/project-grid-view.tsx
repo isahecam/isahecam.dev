be46cb1900f3d1e9a239b01cf2e9d5fc9397aab0
Brandon Hernández <isahecam@gmail.com>
2026-08-17 18:37:05 -0600

    refactor: update ProjectGridView to fetch and display projects

--- src/features/projects/components/project-grid-view.tsx
+++ src/features/projects/components/project-grid-view.tsx
@@ -1,29 +1,17 @@
-import { ProjectCard } from "@/features/projects/components/project-card";
+import { getLocale } from "next-intl/server";
 
-export function ProjectGridView() {
+import { ProjectCard } from "@/features/projects/components/project-card";
+import { getAllProjects } from "@/features/projects/data/get-all-projects";
+
+export async function ProjectGridView() {
+  const locale = await getLocale();
+  const items = await getAllProjects(locale);
+
   return (
     <div className="flex flex-col gap-6">
-      {/* {items.map((project) => ( */}
-      <ProjectCard
-        // key={project._id}
-        project={{
-          _id: "1",
-          title: "Project Title",
-          tagline: "Project Tagline",
-          slug: "project-slug",
-          description: "Project Description",
-          imageGallery: [],
-          coverImage: {
-            _key: "cover-image-key",
-            url: "https://assets.isahecam.dev/images/ascii-art-hero-background.png",
-            alt: "Cover Image Alt Text",
-          },
-          liveUrl: "https://example.com",
-          publishedAt: "2024-06-01T00:00:00Z",
-          technologies: [],
-        }}
-      />
-      {/* ))} */}
+      {items.map((project) => (
+        <ProjectCard key={project._id} project={project} />
+      ))}
     </div>
   );
 }
