e9218c4b96b6a73f88d439686b554e1a8fe8f0f6
Brandon Hernández <isahecam@gmail.com>
2026-08-18 19:33:49 -0600

    fix: update import to use clientEnv for Sanity client configuration

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,10 +1,10 @@
 import { createClient } from "next-sanity";
 
-import { env } from "@/config/env/client";
+import { clientEnv } from "@/config/env/client";
 
 export const sanityClient = createClient({
-  projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: clientEnv.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: clientEnv.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2026-05-15",
   useCdn: false,
 });
